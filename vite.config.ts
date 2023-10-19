5f2fc33a6e12f16ac4749cc659007cc588c1a33e
thegears <semh.1362@gmail.com>
2023-10-19 22:26:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),WindiCSS(),vuetify({ autoImport: true })],
+  base: "/notearsv3/"
 })
